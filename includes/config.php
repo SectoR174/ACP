cbef7bede5b4f2953e985ea9c93642b1e603a3b9
SectoR174 <Svetlakov.91@mail.ru>
2012-05-26 15:53:52 +0500

    config

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?
+	$site_title = "ACP";						//Титул сайта вервера
+	$mysql_host = "localhost";					//Адрес mysql сервера
+	$mysql_port = "3306";						//Порт mysql сервера
+	$mysql_login = "root";						//Логин mysql сервера
+	$mysql_password = "root";					//Пароль mysql сервера
+	$mysql_db_realmd = "auth";					//Название базы аккаунтов
+	$mysql_db_char = "characters";				//Название базы персоонажей
+	$mysql_db_web = "web";						//Название базы сайта
+	$mysql_db_world	="world";					//Название базы мира
+	$database_encoding = "UTF8";				//кодировка базы
+?>
