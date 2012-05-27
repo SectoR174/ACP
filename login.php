262d51f63982b7650e7e5a312b5d421034663345
SectoR174 <Svetlakov.91@mail.ru>
2012-05-27 08:42:38 +0500

    loginform

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<?
+echo'
 		<form method="post" action="login.php">
 			<table>
 			<tr>
@@ -13,3 +15,41 @@
 			</tr>
 			</table>
 		</form>
+';
+if (isset($_POST['login'])) {
+   $passwordHash = SHA1(CONCAT(UPPER($_POST['login']),':',UPPER($_POST['password'])));
+   $login = $_POST['login'];
+   // Проверка логина на плохие смиволы
+   if (!preg_match("/^\w{3,}$/", $login)) {
+      die('Неправильный логин!');
+   }
+   $link = mysql_connect('localhost',$dbuser,$dbpass);
+   if (!$link) {
+      die('Не удалось соединиться с БД');
+   }else{
+      mysql_select_db('authorize', $link);
+      $res = mysql_query("SELECT status FROM users WHERE login='$login'", $link);
+      // Есть ли пользователь с таким логином?
+      if (mysql_num_rows($res) < 1) {
+         mysql_close($link);
+         die('Такого пользователя нет!');
+      }
+      // Какой статус у пользователя?
+      if (mysql_result($res, 0) != 1) {
+         mysql_close($link);
+         die('Логин не активирован!');
+      }
+      // Стартуем сессию и записываем логин в суперглобальный массив $_SESSION
+      session_start();
+      $_SESSION['user'] = $login;
+      mysql_close($link);
+      // Если определена страница с которой мы пришли,
+      // на нее и переадресуем, либо на главную
+      if (isset($_SERVER['HTTP_REFERER'])) {
+         header ("location: ".$_SERVER['HTTP_REFERER']);
+      }else {
+         header ("location: index.php");
+      }
+   }
+}
+?>
