de0c9131c10cc2cf792cc33c6f7a303a2d1f04f9
SectoR174 <Svetlakov.91@mail.ru>
2012-05-28 07:12:16 +0500

    loginform

--- login.php
+++ login.php
@@ -1,4 +1,8 @@
 <?
+include "./includes/config.php";
+include "./includes/functions.php";
+mysql_connect("$mysql_host","$mysql_login","$mysql_password");
+mysql_select_db("$mysql_db_realmd");
 echo'
 		<form method="post" action="login.php">
 			<table>
@@ -16,40 +20,28 @@ echo'
 			</table>
 		</form>
 ';
-if (isset($_POST['login'])) {
-   $passwordHash = SHA1(CONCAT(UPPER($_POST['login']),':',UPPER($_POST['password'])));
-   $login = $_POST['login'];
-   // Проверка логина на плохие смиволы
-   if (!preg_match("/^\w{3,}$/", $login)) {
-      die('Неправильный логин!');
-   }
-   $link = mysql_connect('localhost',$dbuser,$dbpass);
-   if (!$link) {
-      die('Не удалось соединиться с БД');
-   }else{
-      mysql_select_db('authorize', $link);
-      $res = mysql_query("SELECT status FROM users WHERE login='$login'", $link);
-      // Есть ли пользователь с таким логином?
-      if (mysql_num_rows($res) < 1) {
-         mysql_close($link);
-         die('Такого пользователя нет!');
-      }
-      // Какой статус у пользователя?
-      if (mysql_result($res, 0) != 1) {
-         mysql_close($link);
-         die('Логин не активирован!');
-      }
-      // Стартуем сессию и записываем логин в суперглобальный массив $_SESSION
-      session_start();
-      $_SESSION['user'] = $login;
-      mysql_close($link);
-      // Если определена страница с которой мы пришли,
-      // на нее и переадресуем, либо на главную
-      if (isset($_SERVER['HTTP_REFERER'])) {
-         header ("location: ".$_SERVER['HTTP_REFERER']);
-      }else {
-         header ("location: index.php");
-      }
-   }
+include ("./include/core.php");
+session_start();
+
+$ulogin = trim(mysql_escape_string($_POST['login']));
+$upass = trim(mysql_escape_string($_POST['pass']));
+
+if($cAcc->CheckAccountExist($ulogin)) {
+                $uarray = $cAcc->GetAccIdPassMail($ulogin);
+                $pass = strtolower($uarray['sha_pass_hash']);
+                $sph =  sha1(strtoupper("$ulogin") .":". strtoupper("$upass"));
+                if($sph==$pass) {
+                                $_SESSION["loginmode"]=1;
+                                $_SESSION["realmid"]=1;
+                                $_SESSION["uid"]=$uarray['id'];
+                                $_SESSION["accname"]=$ulogin;
+                                header("Location: index.php");
+                } else {
+                                sleep(2);
+                                header("Location: login.php?login=false");
+                }
+} else {
+                sleep(2);
+                header("Location: login.php?login=false");
 }
-?>
+?> 
